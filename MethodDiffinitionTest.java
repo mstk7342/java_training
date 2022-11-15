ba343115cb79d621f6d498d0e7e1c91852d2bc7a
mstk7342 <m7s3t4k228@gmail.com>
2022-11-15 23:38:37 +0900

    first commit

--- MethodDiffinitionTest.java
+++ MethodDiffinitionTest.java
@@ -1,28 +1,42 @@
-package Udemy;
 public class MethodDiffinitionTest {
   public static void main(String[] args){ //エントリーポイント・・・プログラムの入口
     System.out.println("mainメソッド開始");
-    // int number1 = 5;
-    // int number2 = 10;
-    int sum = 0;
+    int intSum = 0;
+    double doubleSum = 0;
+    int number1 = Integer.parseInt(args[0]);
+    int number2 = Integer.parseInt(args[1]);
+    int number3 = Integer.parseInt(args[2]);
     //足し算する処理→結果の戻り値をsumに代入
     //sum = add(number1, number2);  //返された値をセットしてあげる。
-    sum = add((int)5.0, 10);
+    //sum = add((int)5.0, 10);
+    intSum = add(number1, number2, number3);
     //足し算した結果を画面に表示する処理
-    printSum(sum);
-    sum = add(30, 70 );
+    printSum(intSum);
+    doubleSum = add(30.5,70.5);
     //足し算した結果を画面に表示する処理
-    printSum(sum);
+    printSum(doubleSum);
     System.out.println("mainメソッド終了");
   }
 
-  public static int add(int number1, int number2){
-    System.out.println("addメソッド開始");
+  public static int add(int number1, int number2, int number3){
+    System.out.println("int型のaddメソッド開始");
     System.out.println("number1:" + number1);
     System.out.println("number2:" + number2);
+    System.out.println("number3:" + number3);
     int sum = 0;
-    sum = number1 + number2;
-    System.out.println("addメソッド終了");
+    sum = number1 + number2 + number3;
+    System.out.println("int型addメソッド終了");
+    //メソッドの閉じカッコ：呼び出したところに戻る
+    return sum;
+  }
+
+  public static double add(double number1, double number2){
+    System.out.println("double型のaddメソッド開始");
+    System.out.println("number1:" + number1);
+    System.out.println("number2:" + number2);
+    double sum = 0;
+    sum = number1 + number2  ;
+    System.out.println("double型のaddメソッド終了");
     //メソッドの閉じカッコ：呼び出したところに戻る
     return sum;
   }
@@ -40,8 +54,14 @@ public class MethodDiffinitionTest {
   // }
 
   public static void printSum(int sum){
-    System.out.println("printSumメソッド開始");
+    System.out.println("int型のprintSumメソッド開始");
     System.out.println("sum" + sum);
-    System.out.println("printSumメソッド終了");
+    System.out.println("int型のprintSumメソッド終了");
+  }
+
+  public static void printSum(double sum){
+    System.out.println("double型のprintSumメソッド開始");
+    System.out.println("sum" + sum);
+    System.out.println("double型のprintSumメソッド終了");
   }
 }
